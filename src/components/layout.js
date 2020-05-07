c898c05509b3d03a6f980abef6f4216ad4731e85
Kevin Tran <sesouk@gmail.com>
2020-05-07 14:52:19 -0600

    added instagram icon

--- src/components/layout.js
+++ src/components/layout.js
@@ -8,15 +8,17 @@
 import React from "react"
 import PropTypes from "prop-types"
 import logo from '../images/jadasMochiDonuts_V1.png'
-
+import ig from '../images/glyph-logo_May2016.png'
 import "./styles/layout.scss"
 
 const Layout = ({ children }) => {
   return (
     <>
+    <a href='https://www.instagram.com/jadasmochidonuts'><img style={{ width: `29px`, marginTop: `.5rem`, marginLeft: `.5rem` }} src={ig} alt='Instagram' /></a>
       <div
         style={{
           margin: `2rem auto`,
+          marginTop: `0`,
           maxWidth: 960,
           padding: `0 1.0875rem 1.45rem`,
         }}
