7151c2b47f2b2fb543076866576599292e85c82e
Kevin Tran <sesouk@gmail.com>
2020-09-01 13:49:56 -0600

    added about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -4,16 +4,14 @@ import { Link } from "gatsby"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-const Success = () => (
+const About = () => (
   <Layout>
     <SEO title="Page two" />
     <div style={{ textAlign: `center` }}>
 
-    <h1>Your order has been placed successfully!</h1>
-    <p>Awesome, we've received your request and will contact you soon for payment and address for delivery (if applicable). Please allow 24-48 hours for your donuts to be ready. Thank you for the support!</p>
-    <Link style={{ color: `black`, textDecoration: `none` }} to="/">Go back to the homepage</Link>
+    <p>Jada’s passion for baking started when she was just 6 years old. She used to ask for baking supplies when asked what she wanted for birthdays and holidays. While most other kids were playing with toys, Jada was baking a variety of goodies to share with family and friends. Jada just started her junior year of high school, and we started this business as a way to help her raise money for her upcoming cheer season. We never anticipated all of the love and support, but we are very grateful! We can’t wait to continue sharing the love through our mochi donuts 💜</p>
     </div>
   </Layout>
 )
 
-export default Success
+export default About
