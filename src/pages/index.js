052ffd6b32db71a647af82d45afc0568168ec7d6
Kevin Tran <sesouk@gmail.com>
2020-09-15 15:28:10 -0600

    more css

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,35 +3,36 @@ import React, { useState } from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import donuts from '../images/donuts.jpg'
-import donutsFancy from '../images/donutsFancy.png'
 import donutsToGo from '../images/donutsToGo.jpg'
 import donutsClose from '../images/donutsClose.jpg'
 import donutsPacked from '../images/donutsPacked.jpg'
+import logo from '../images/jadasMochiDonuts_V1.png'
 
 const IndexPage = () => {
-const [ slide, setSlide ] = useState(1)
+  const [slide, setSlide] = useState(1)
 
-const prev = () => {
-  if( slide === 1 ) {
-    setSlide( 4 )
+  const prev = () => {
+    if (slide === 1) {
+      setSlide(4)
+    }
+    else {
+      setSlide(slide - 1)
+    }
   }
-  else {
-    setSlide( slide - 1 )
-  }
-}
 
-const next = () => {
-  if( slide === 4 ) {
-    setSlide( 1 )
+  const next = () => {
+    if (slide === 4) {
+      setSlide(1)
+    }
+    else {
+      setSlide(slide + 1)
+    }
   }
-  else{
-    setSlide( slide + 1 )
-  }
-}
-return (
-  <Layout>
-    <SEO title="Home" />
-    <div className='body'>
+  return (
+    <Layout>
+      <SEO title="Home" />
+      <div className='landing'>
+      <div className='slide'>
         {slide === 1
           ? <div className="slideshow">
             <div className="slideshow__slides slideshow__fade">
@@ -60,12 +61,17 @@ return (
             </div>
           </div>
           : null}
-      <div className='btn'>
-      <button className="btn__prev" onClick={prev}>&#10094;</button>
-      <button className="btn__next" onClick={next}>&#10095;</button>
+        <div className='btn'>
+          <button className="btn__prev" onClick={prev}>&#10094;</button>
+          <button className="btn__next" onClick={next}>&#10095;</button>
+        </div>
       </div>
-       </div> 
-  </Layout>
+        <div className="welcome">
+          <img className="welcome__img" src={logo} alt="Jada's Mochi Donuts Logo"/>
+          <p className="welcome__text">Welcome to Jada’s Mochi Donuts! Our donuts are homemade and freshly made for every order. Treat yourself and your loved ones to a deliciously chewy treat 💜</p>
+        </div>
+        </div>
+    </Layout>
   )
 }
 
