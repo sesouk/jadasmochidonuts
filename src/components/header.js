ef878d46680051d11e17663bcd2ef9f62ad2f35b
Kevin Tran <sesouk@gmail.com>
2020-09-16 16:41:11 -0600

    almost done with navbar

--- src/components/header.js
+++ src/components/header.js
@@ -5,35 +5,24 @@ import jmd from '../images/jmd.png'
 import ig from '../images/glyph-logo_May2016.png'
 
 const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: ` #b1a6c8`,
-      borderBottom: `solid #f8b4c0 2px`
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        padding: `1.45rem`,
-        display: 'grid',
-        gridTemplateColumns: '.35fr 1fr' 
-      }}
-    >
-      {/* <img src={jmd} alt="Jada's Mochi Donuts" style={{
-        width: '300px',
-        alignSelf: 'center'
-      }}/> */}
-      <p>Jada's Mochi Donuts</p>
-      <ul style={{
-        display: 'grid',
-        gridTemplateColumns: '1fr 1fr 1fr 1fr 1fr'
-      }}>
-        <li style={{listStyleType: 'none', justifySelf: 'center'}}><Link style={{color: 'black', textDecoration: 'none'}} to='/'>Home</Link></li>
-        <li style={{listStyleType: 'none', justifySelf: 'center'}}><Link style={{color: 'black', textDecoration: 'none'}} to='/about'>About</Link></li>
-        <li style={{listStyleType: 'none', justifySelf: 'center'}}><Link style={{color: 'black', textDecoration: 'none'}} to='/flavors'>Flavors</Link></li>
-        <li style={{listStyleType: 'none', justifySelf: 'center'}}><Link style={{color: 'black', textDecoration: 'none'}} to='/order'>Ordering</Link></li>
-        <li style={{listStyleType: 'none', justifySelf: 'center'}}><a href='https://www.instagram.com/jadasmochidonuts'><img style={{ width: `18.67px`}} src={ig} alt='Instagram' /></a></li>
+  <header className='header'>
+    <div className='navbar'>
+      <img className='navbar__img' src={jmd} alt="Jada's Mochi Donuts"/>
+      <ul className='navbar__linkHolder'>
+        <li className='navbar__links'><Link style={{color: 'black', textDecoration: 'none'}} to='/'>Home</Link></li>
+        <li className='navbar__links'><Link style={{color: 'black', textDecoration: 'none'}} to='/about'>About</Link></li>
+        <li className='navbar__links'><Link style={{color: 'black', textDecoration: 'none'}} to='/flavors'>Flavors</Link></li>
+        <li className='navbar__links'><Link style={{color: 'black', textDecoration: 'none'}} to='/order'>Ordering</Link></li>
+        <li className='navbar__links'><a href='https://www.instagram.com/jadasmochidonuts'><img style={{ width: `18.67px`}} src={ig} alt="Click for Jada's Mochi Donuts' Instagram Account" /></a></li>
       </ul>
+      <div className='navbar__more' >Click Here</div>
+      <div className='navbar__mobileHolder'>
+        <div className='navbar__links'><Link style={{color: 'black', textDecoration: 'none'}} to='/'>Home</Link></div>
+        <div className='navbar__links'><Link style={{color: 'black', textDecoration: 'none'}} to='/about'>About</Link></div>
+        <div className='navbar__links'><Link style={{color: 'black', textDecoration: 'none'}} to='/flavors'>Flavors</Link></div>
+        <div className='navbar__links'><Link style={{color: 'black', textDecoration: 'none'}} to='/order'>Ordering</Link></div>
+        <div className='navbar__links'><a href='https://www.instagram.com/jadasmochidonuts'><img style={{ width: `18.67px`}} src={ig} alt="Click for Jada's Mochi Donuts' Instagram Account" /></a></div>
+      </div>
     </div>
   </header>
 )
