93e1cef528739ea70e0bcede898af712f9da613b
Kevin Tran <sesouk@gmail.com>
2020-09-17 14:38:26 -0600

    added changes

--- src/pages/order.js
+++ src/pages/order.js
@@ -28,8 +28,8 @@ const Order = () => {
         {!drawer ? <ul className='body__info--list'>
           <li className='body__info--list--items'>$17 for 1 dozen</li>
           <li className='body__info--list--items'>$15 per dozen when ordering 2 or more</li>
-          <li className='body__info--list--items'>Curbside pickup is free</li>
-          <li className='body__info--list--items'>Delivery fee of $2 - $8 will apply depending on location</li>
+          <li className='body__info--list--items'>Curbside pickup is free (Southeast Aurora)</li>
+          <li className='body__info--list--items'>Delivery fee of $4 - $10 will apply depending on location</li>
         </ul> : null }
       </div>
       <div className="orderContainer">
